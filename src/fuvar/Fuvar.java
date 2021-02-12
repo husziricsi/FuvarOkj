b9ecf2e14aa916788809fa0b75cd49979349b90e
RichardHuszar <huszirics10@gmail.com>
2021-02-12 11:54:24 +0100

    első feladat

---
+++ src/fuvar/Fuvar.java
@@ -0,0 +1,16 @@
+package fuvar;
+
+public class Fuvar {
+ private int azonosito;
+    private String indulas;
+    private int idotartam;
+    private double tavolsag;
+    private double viteldij;
+    private double borravalo;
+    private String fizetesmod;
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}
