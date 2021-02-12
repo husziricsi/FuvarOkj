835cc23ea9686ebe4964d155e26817531bbf86ff
RichardHuszar <huszirics10@gmail.com>
2021-02-12 12:58:36 +0100

    hatodik feladat

--- src/fuvar/Fuvarprogram.java
+++ src/fuvar/Fuvarprogram.java
@@ -9,51 +9,56 @@ import java.util.List;
 import java.util.Map;
 
 public class Fuvarprogram {
- 
-    
+
     public static void main(String[] args) throws IOException {
         List<String> sorok = Files.readAllLines(Paths.get("fuvar.csv"));
         ArrayList<Fuvar> fuvarok = new ArrayList();
-        
+
         for (int i = 1; i < sorok.size(); i++) {
             fuvarok.add(new Fuvar(sorok.get(i)));
         }
-        
+
         int fuvarokszama = 0;
         for (int i = 0; i < fuvarok.size(); i++) {
             fuvarokszama++;
         }
-        System.out.print("3.feladat: " +fuvarokszama+" fuvar");
+        System.out.print("3.feladat: " + fuvarokszama + " fuvar");
         double osszesbev = 0;
         int osszfuvar = 0;
         System.out.println("4. feladat");
         for (int i = 0; i < fuvarok.size(); i++) {
-            if(fuvarok.get(i).getAzonosito() == 6185){
-            osszfuvar++;
-           osszesbev += fuvarok.get(i).getViteldij();
+            if (fuvarok.get(i).getAzonosito() == 6185) {
+                osszfuvar++;
+                osszesbev += fuvarok.get(i).getViteldij();
             }
-            
+
         }
-        System.out.println(osszfuvar+ "fuvar alatt: " + osszesbev+ "$");
+        System.out.println(osszfuvar + "fuvar alatt: " + osszesbev + "$");
         System.out.println("5. feladat");
-        
-        HashMap<String , Integer> fizetesmodok = new HashMap<>();
+
+        HashMap<String, Integer> fizetesmodok = new HashMap<>();
         for (int i = 0; i < fuvarok.size(); i++) {
             String kulcs = fuvarok.get(i).getFizetesmod();
-            if(!fizetesmodok.containsKey(kulcs)){
+            if (!fizetesmodok.containsKey(kulcs)) {
                 fizetesmodok.put(kulcs, 1);
-            }else {
+            } else {
                 int ertek = fizetesmodok.get(kulcs);
                 fizetesmodok.put(kulcs, ++ertek);
             }
         }
-        
+
         for (Map.Entry<String, Integer> entry : fizetesmodok.entrySet()) {
             String key = entry.getKey();
             Integer value = entry.getValue();
-            System.out.println("\t"+key +": "+ value + " fuvar");
+            System.out.println("\t" + key + ": " + value + " fuvar");
         }
-        
-    }
-    
+
+        System.out.println("6. feladat");
+        double ossztav = 0;
+        for (int i = 0; i < fuvarok.size(); i++) {
+            ossztav += fuvarok.get(i).getTavolsag();
+        }
+        System.out.printf("%.2f km ",(ossztav*1.6));
+    } 
+
 }
