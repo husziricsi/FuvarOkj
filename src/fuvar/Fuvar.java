d2fdee22ab3d4831a8cb816b8b092cbc94c43e17
RichardHuszar <huszirics10@gmail.com>
2021-02-12 12:14:48 +0100

    Második feladat

--- src/fuvar/Fuvar.java
+++ src/fuvar/Fuvar.java
@@ -1,7 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package fuvar;
 
 public class Fuvar {
- private int azonosito;
+    
+    private int azonosito;
     private String indulas;
     private int idotartam;
     private double tavolsag;
@@ -9,8 +15,15 @@ public class Fuvar {
     private double borravalo;
     private String fizetesmod;
     
-    public static void main(String[] args) {
-       
+    public Fuvar(String sor) {
+        String[] s = sor.split(";");
+        this.azonosito = Integer.parseInt(s[0]);
+        this.indulas = s[1];
+        this.idotartam = Integer.parseInt(s[2]);
+        this.tavolsag = Double.parseDouble(s[3]);
+        this.viteldij = Double.parseDouble(s[4]);
+        this.borravalo = Double.parseDouble(s[5]);
+        this.fizetesmod = s[6];
     }
     
 }
---
+++ src/fuvar/Fuvarprogram.java
@@ -0,0 +1,20 @@
+package fuvar;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fuvarprogram {
+ 
+    
+    public static void main(String[] args) throws IOException {
+       List<String> sorok = Files.readAllLines(Paths.get("fuvar.csv"));
+        ArrayList<Fuvar> fuvarok = new ArrayList();
+        for (int i = 1; i < sorok.size(); i++) {
+            fuvarok.add(new Fuvar(sorok.get(i)));
+        }
+    }
+    
+}
